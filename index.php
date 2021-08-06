1b4eb8233bdaabc3b49321a9404c76d4512667d5
yitd <55494975+yitd@users.noreply.github.com>
2021-08-07 07:17:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ header("Access-Control-Allow-Origin:*");
 header("Access-Control-Allow-Methods:GET");
 header("Access-Control-Allow-Headers:x-requested-with,content-type");
 header("Content-Type:text/html,application/json; charset=utf-8");
-$domain = getTopHost($_GET['domain']);
+if ($_GET['domain']) {
+    $domain = getTopHost($_GET['domain']);
+} else {
+    exit("缺少参数");
+}
 $timeStamp = time();
 $authKey = md5("testtest" . $timeStamp);
 $token = json_decode(curl_post("https://hlwicpfwc.miit.gov.cn/icpproject_query/api/auth", "authKey=$authKey&timeStamp=$timeStamp", "application/x-www-form-urlencoded;charset=UTF-8", "0"));
